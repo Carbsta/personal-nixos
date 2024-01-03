309aed5ad450121ed4ccf566634fdf93b2f764dc
carbsta <carbsta@gmail.com>
2024-01-03 21:09:04 +0000

    set zsh as default shell

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -117,6 +117,11 @@
     wget
     git
   ];
+  
+  # set zsh as default shell  
+  environment.shells = with pkgs; [ zsh ];
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
