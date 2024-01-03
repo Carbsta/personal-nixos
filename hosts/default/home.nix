8a729b0be1302496e532eb601b5a11bc77f5f0c1
carbsta <carbsta@gmail.com>
2024-01-03 21:45:12 +0000

    readded discord

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -37,6 +37,7 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    pkgs.discord  
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
