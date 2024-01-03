c82690062fab78d6b6a926d6c0c4dfbf3c724caa
carbsta <carbsta@gmail.com>
2024-01-03 21:41:46 +0000

    allow unfree in home.nix

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -14,6 +14,9 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # Enable unfree packages
+  nixpkgs.config.allowUnfree = true; 
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -34,7 +37,6 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-    pkgs.discord 
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
